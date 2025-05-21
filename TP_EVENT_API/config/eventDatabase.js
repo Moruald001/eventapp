966b61c159dc7576d10c814a8c7c6532d2e84fa2
romuald <sodouessodina@gmail.com>
2025-05-21 18:27:17 +0000

    test 8

--- TP_EVENT_API/config/eventDatabase.js
+++ TP_EVENT_API/config/eventDatabase.js
@@ -8,8 +8,8 @@ const sequelize = new Sequelize(
   process.env.DB_USER, // Utilisateur de la base
   process.env.DB_PASSWORD, // Mot de passe
   {
-    host: "postgres", // Adresse du serveur PostgreSQL
-    port: 5432, // Port PostgreSQL
+    host: process.env.DB_HOST || "postgres", // Adresse du serveur PostgreSQL
+    port: process.env.DB_PORT || 5432, // Port PostgreSQL
     dialect: "postgres", // Spécifie qu'on utilise PostgreSQL
     logging: false, // Désactive les logs si besoin, pour un environnement de production
   }
